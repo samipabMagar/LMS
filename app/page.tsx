ea653a90be928468cd5f4b04272e444b106f21c8
samipabMagar <samipab98@gmail.com>
2026-06-17 18:31:06 +0545

    Add shadcn/ui, and custom global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <>
+      <h1 className="text-3xl font-bold">Welcome to Converso</h1>
+      <Button>samipab</Button>
+    </>
   )
 }
 
